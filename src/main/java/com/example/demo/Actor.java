84a3c0c383947f2e5712d939de04a785cf3f5163
nakelia roseman <nakelia78@gmail.com>
2017-08-22 23:32:16 -0400

    first commit(uploading images with cloudinary)

---
+++ src/main/java/com/example/demo/Actor.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO
+    private long id;
+    private String name;
+    private String realname;
+    private String headshots;
+
+}
